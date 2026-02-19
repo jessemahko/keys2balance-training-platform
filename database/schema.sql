e098c0b82ebf1c32a72a026d9947b22f78a2e819
Yanli <yanli.zhang@student.lut.fi>
2026-02-19 20:10:45 +0200

    update

--- database/schema.sql
+++ database/schema.sql
@@ -16,6 +16,7 @@ CREATE TABLE IF NOT EXISTS users (
 
   email          TEXT NOT NULL UNIQUE,
   password_hash  TEXT NOT NULL,
+  role           TEXT NOT NULL DEFAULT 'participant',
 
   -- profile fields
   first_name     TEXT,
