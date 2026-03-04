ef2ee7d350d2a28604db48e46322a73c563715a4
Yanli <yanli.zhang@student.lut.fi>
2026-03-04 13:26:28 +0200

    remove unnecessary user routes (email/username lookup)

--- backend/controllers/user/userRoute.js
+++ backend/controllers/user/userRoute.js
@@ -5,8 +5,6 @@ const middleware = require('../../utils/middleware')
 router.get('/me', userController.getMe)
 
 router.get('/', middleware.authorizeRoles('admin'), userController.getUsers)
-router.get('/email/:email', middleware.authorizeRoles('admin'), userController.getUserByEmail)
-router.get('/username/:username', middleware.authorizeRoles('admin'), userController.getUserByUsername)
 router.post('/', middleware.authorizeRoles('admin'), userController.createUser)
 
 module.exports = router
