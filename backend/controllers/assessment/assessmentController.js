07dab29b38594f8ecd70e6501f50d54723eeadb0
VasiliiP05 <pudyakov.vasily@gmail.com>
2026-03-04 22:08:51 +0200

    Added user check in assessment controller, refactored assessment route, added middleware.userextractor to assessnents in app.js

--- backend/controllers/assessment/assessmentController.js
+++ backend/controllers/assessment/assessmentController.js
@@ -1,28 +1,27 @@
 const Assessment = require('../models/assessment');
 
 const AssessmentController = {
-  // Create a new assessment
-  async createAssessment(req, res) {
-    
+  async createAssessment(req) {
+    const user = req.user
+    if (!user) return res.status(401).json({ error: 'invalid token' })
+
     const { lessonId, title, assessmentJson } = req.body;
-    const assessment = await Assessment.create({ lessonId, title, assessmentJson });
-    res.json(assessment); // return created assessment
-  
+    return await Assessment.create({ lessonId, title, assessmentJson });
   },
 
-  // Get assessment by ID
-  async getAssessment(req, res) {
+  async getAssessment(req) {
+    const user = req.user
+    if (!user) return res.status(401).json({ error: 'invalid token' })
+      
     const { id } = req.params;
     const assessment = await Assessment.getById(id);
-    if (!assessment) {
-      return res.status(404).json({ error: 'Assessment not found' });
-    }
+    if (!assessment) return null;
 
-    // Remove correct answers before sending to frontend
     const sanitized = JSON.parse(JSON.stringify(assessment.assessment_json));
     sanitized.questions?.forEach(q => delete q.correctOptionId);
     assessment.assessment_json = sanitized;
-    res.json(assessment);
+
+    return assessment;
   }
 };
 
--- backend/controllers/assessment/assessmentRoute.js
+++ backend/controllers/assessment/assessmentRoute.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
-const AssessmentController = require('../controllers/assessmentController');
+const AssessmentController = require('./assessmentController');
+//create assessment
+router.post('/', async (req, res) => {
+  const assessment = await AssessmentController.createAssessment(req);
+  res.json(assessment);
+});
 
-// Create a new assessment
-router.post('/', AssessmentController.createAssessment);
-
-// Get assessment by ID
-router.get('/:id', AssessmentController.getAssessment);
+//get assessment
+router.get('/:id', async (req, res) => {
+  const assessment = await AssessmentController.getAssessment(req);
+  if (!assessment) return res.status(404).json({ error: 'Assessment not found' });
+  res.json(assessment);
+});
 
 module.exports = router;
