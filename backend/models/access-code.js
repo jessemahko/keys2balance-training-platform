af6132228f3131af3c9ae5e10546a8f5345bbf99
peogway <duchungbeo2004@gmail.com>
2026-03-15 12:47:34 +0200

    refactor: remove findAllAndCleanup function and adjust access code retrieval logic

--- backend/models/access-code.js
+++ backend/models/access-code.js
@@ -1,11 +1,14 @@
 const { pool } = require('../utils/config')
 
-const findAllAndCleanup = async () => {
-	await pool.query(`
-		DELETE FROM access_codes
-		WHERE available = false
-		OR created_at < NOW() - INTERVAL '1 day'
-	`)
+const findByAccessCode = async (code) => {
+	const res = await pool.query(
+		`SELECT * FROM access_codes WHERE code = $1 AND is_available = true`,
+		[code],
+	)
+	return res.rows[0] || null
+}
+
+const findAll = async () => {
 	const res = await pool.query(`SELECT * FROM access_codes`)
 	return res.rows
 }
@@ -22,9 +25,17 @@ const deleteAccessCode = async (id) => {
 	await pool.query(`DELETE FROM access_codes WHERE code_id = $1`, [id])
 }
 
+const updateAvailability = async (code) => {
+	await pool.query(
+		`UPDATE access_codes SET is_available = false WHERE code = $1`,
+		[code],
+	)
+}
 module.exports = {
-	findAllAndCleanup,
+	findByAccessCode,
+	findAll,
 	create,
 	delete: deleteAccessCode,
+	updateAvailability,
 }
 
