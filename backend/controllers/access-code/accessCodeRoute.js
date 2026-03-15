9891dcc1f53960efb539ab6807cfda22c3b7b438
peogway <duchungbeo2004@gmail.com>
2026-03-15 12:47:58 +0200

    refactor: update access code retrieval to use getAllAccessCodes

--- backend/controllers/access-code/accessCodeRoute.js
+++ backend/controllers/access-code/accessCodeRoute.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const AccessCodeController = require('./accessCodeController')
-router.get('/', AccessCodeController.getAllAccessCodesAndCleanup)
+router.get('/', AccessCodeController.getAllAccessCodes)
 router.post('/', AccessCodeController.createAccessCode)
 router.delete('/:id', AccessCodeController.deleteAccessCode)
 module.exports = router
