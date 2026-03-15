8e2fe5f6f4aa9107dba95adc21bc35fc327d219b
peogway <duchungbeo2004@gmail.com>
2026-03-15 13:28:20 +0200

    refactor: remove unused 'getMe' route from userRouter

--- backend/controllers/user/userRoute.js
+++ backend/controllers/user/userRoute.js
@@ -2,8 +2,6 @@ const router = require('express').Router()
 const userController = require('./userController')
 const middleware = require('../../utils/middleware')
 
-router.get('/me', middleware.userExtractor, userController.getMe)
-
 router.get('/', middleware.authorizeRoles('admin'), userController.getUsers)
 router.post('/', middleware.authorizeRoles('admin'), userController.createUser)
 
