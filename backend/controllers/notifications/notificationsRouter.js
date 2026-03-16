1258f3d1f7bde4dc856efffaf27e34fc69a0882a
peogway <duchungbeo2004@gmail.com>
2026-03-16 16:34:38 +0200

    refactor: update notification route to use a generic path for updates

--- backend/controllers/notifications/notificationsRouter.js
+++ backend/controllers/notifications/notificationsRouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const NotificationController = require('./notificationController')
 
 router.get('/', NotificationController.getAllNotifications)
-router.put('/:id', NotificationController.updateNotification)
+router.put('/', NotificationController.updateNotification)
 router.delete('/:id', NotificationController.deleteNotification)
 
 module.exports = router
