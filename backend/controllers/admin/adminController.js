85348bb371ab67596f7a011fd71e8a36348cda29
peogway <duchungbeo2004@gmail.com>
2026-03-16 06:57:30 +0200

    refactor: remove admin controller to streamline backend structure

--- backend/controllers/admin/adminController.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const User = require('../../models/user')
-
-router.get('/users', async (req,res) => {
-	const users = await User.findAll()
-	res.json(users)
-})
-
-module.exports = router
