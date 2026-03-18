a30eb92d36a8661ffaca8017ac1229db2b1ced2c
peogway <duchungbeo2004@gmail.com>
2026-03-18 04:12:51 +0200

    refactor: restructure main entry point to include Redux provider and router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { Provider } from 'react-redux'
+import App from './App'
+import store from './store'
+import { BrowserRouter as Router } from 'react-router-dom'
+import './i18n'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<Provider store={store}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>,
 )
