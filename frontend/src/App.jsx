be31a57023c5165fb08759e6482c021569719095
peogway <duchungbeo2004@gmail.com>
2026-03-18 05:46:46 +0200

    feat: implement authentication flow with user state management and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, cloneElement } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-function App() {
-  const [count, setCount] = useState(0)
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link,
+	Outlet,
+	Navigate,
+	useNavigate,
+	useLocation,
+} from 'react-router-dom'
+import Authentication from './pages/authentication/Authentication'
+import { setUserFn, rmUserFn } from './reducers/userReducer'
+import { setToken, isTokenExpired } from './services/login'
+import { useTranslation } from 'react-i18next'
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+	const dispatch = useDispatch()
+	const user = useSelector((state) => state.user)
+	const navigate = useNavigate()
+	const location = useLocation()
+	const { t, i18n } = useTranslation()
+
+	useEffect(() => {
+		const loggedUserJSON = window.localStorage.getItem('loggedUser')
+		if (!loggedUserJSON) {
+			navigate('authentication')
+		}
+
+		const user = JSON.parse(loggedUserJSON)
+		if (isTokenExpired(user.token)) {
+			dispatch(rmUserFn())
+			window.localStorage.removeItem('loggedUser')
+		} else {
+			dispatch(setUserFn(user))
+			setToken(user.token)
+		}
+	}, [])
+
+	return (
+		<div>
+			<Routes>
+				{/* Public Route */}
+				<Route path='/authentication' element={<Authentication />} />
+
+				{/* Protected Routes */}
+				<Route
+					element={
+						user ? <Outlet /> : <Navigate replace to='/authentication' />
+					}
+				>
+					<Route path='/' element={<Navigate replace to='/dashboard' />} />
+					<Route
+						path='/dashboard'
+						element={<div>{/* dashboard element */}</div>}
+					/>
+				</Route>
+
+				{/* Catch-all Route */}
+				<Route path='*' element={<Navigate replace to='/' />} />
+			</Routes>
+		</div>
+	)
 }
 
 export default App
