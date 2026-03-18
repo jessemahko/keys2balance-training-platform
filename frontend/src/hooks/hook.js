c3033c2a664789be2b3f2c2815a741762de4c179
peogway <duchungbeo2004@gmail.com>
2026-03-18 05:45:27 +0200

    feat: add custom hook for managing form field state

---
+++ frontend/src/hooks/hook.js
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+export const useField = (type, content = '') => {
+	const [value, setValue] = useState(content)
+
+	const onChange = (event) => setValue(event.target.value)
+
+	const remove = () => setValue('')
+
+	return {
+		type,
+		value,
+		onChange,
+		remove,
+	}
+}
+
