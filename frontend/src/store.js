ea9a3ebd8b55ab69ce368a59c9216fd061bd7d56
peogway <duchungbeo2004@gmail.com>
2026-03-18 04:13:18 +0200

    feat: add initial Redux store configuration

---
+++ frontend/src/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+// import reducers
+// example: import counterReducer from './features/counter/counterSlice'
+
+export default configureStore({
+	reducer: {
+		// example: counter: counterReducer,
+	},
+})
+
