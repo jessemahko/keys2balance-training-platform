7269ece0b0d77a708e80f372b3b9e3fcf31c75b3
peogway <duchungbeo2004@gmail.com>
2026-03-18 05:46:20 +0200

    feat: add user reducer with actions for setting, removing, and editing user state

---
+++ frontend/src/reducers/userReducer.js
@@ -0,0 +1,59 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { isTokenExpired, getToken } from '../services/login'
+// import profile from '../services/profile'
+
+const userSlice = createSlice({
+	name: 'user',
+	initialState: null,
+	reducers: {
+		setUser(state, action) {
+			return action.payload
+		},
+		removeUser(state, action) {
+			return null
+		},
+		editUser(state, action) {
+			// console.log(state.token, action.payload.token)
+
+			return { ...state, ...action.payload }
+		},
+	},
+})
+
+export const { setUser, removeUser, editUser } = userSlice.actions
+
+export const setUserFn = (user) => {
+	return (dispatch) => {
+		dispatch(setUser(user))
+	}
+}
+
+export const rmUserFn = () => {
+	return (dispatch) => dispatch(removeUser())
+}
+
+// export const updateAvatar = (pic) => {
+// 	return async (dispatch) => {
+// 		if (isTokenExpired(getToken())) {
+// 			dispatch(rmUserFn())
+// 			return
+// 		}
+// 		const { avatarUrl } = await profile.updateAvatar(pic) // Extract the avatarUrl
+
+// 		dispatch(editUser({ avatarUrl })) // Pass it as a string
+// 	}
+// }
+
+// export const updateProfile = (user) => {
+// 	return async (dispatch) => {
+// 		if (isTokenExpired(getToken())) {
+// 			dispatch(rmUserFn())
+// 			return
+// 		}
+// 		await profile.updateProfile(user)
+// 		dispatch(editUser(user))
+// 	}
+// }
+
+export default userSlice.reducer
+
