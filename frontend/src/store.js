4cff0f44443147aac192fb46313efe9fbf8ce41b
peogway <duchungbeo2004@gmail.com>
2026-03-18 09:20:12 +0200

    feat: add notiReducer to the store configuration

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,9 +2,10 @@ import { configureStore } from '@reduxjs/toolkit'
 
 // import reducers
 import userReducer from './reducers/userReducer'
-
+import notiReducer from './reducers/notiReducer'
 export default configureStore({
 	reducer: {
+		noti: notiReducer,
 		user: userReducer,
 	},
 })
