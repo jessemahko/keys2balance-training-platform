dcb248a979b9d1876b538998d2e9ebc02a767447
peogway <duchungbeo2004@gmail.com>
2026-03-20 12:41:54 +0200

    feat: add avatar_url to createUserWithGoogle for user profile enhancement

--- backend/models/user.js
+++ backend/models/user.js
@@ -72,10 +72,11 @@ const createUserWithGoogle = async ({
 	first_name,
 	last_name,
 	gender,
+	avatar_url,
 }) => {
 	const res = await pool.query(
-		`INSERT INTO users (username, email, password_hash, role, is_verified, first_name, last_name, gender)
-         VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+		`INSERT INTO users (username, email, password_hash, role, is_verified, first_name, last_name, gender, avatar_url)
+         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
          RETURNING *`,
 		[
 			username,
@@ -86,6 +87,7 @@ const createUserWithGoogle = async ({
 			first_name,
 			last_name,
 			gender,
+			avatar_url,
 		],
 	)
 	return res.rows[0]
