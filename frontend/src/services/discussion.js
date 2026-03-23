009ca7a99505df5b4bebb4aae1086d6124a4bfc5
Lord Mahi <mahialjubairtalukder@gmail.com>
2026-03-23 05:25:29 +0200

    fix(discussion): correct pluralization in API base URL

--- frontend/src/services/discussion.js
+++ frontend/src/services/discussion.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { getToken } from './authen/login'
 
-const baseUrl = 'http://localhost:3001/api/discussion'
+const baseUrl = 'http://localhost:3001/api/discussions'
 
 const getConfig = () => ({
 	headers: { Authorization: getToken() },
