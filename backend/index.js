c4db05f0a71bf85546d321a41d52a430bb8079fa
peogway <duchungbeo2004@gmail.com>
2026-04-04 11:19:19 +0300

    fix: Enable serving static files and update server listen address

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const app = require('./app')
 const config = require('./utils/config')
 
 // Start the server and listen on the specified port
-app.listen(config.PORT, () => {
+app.listen(config.PORT, '0.0.0.0', () => {
 	console.log(`Server running on port ${config.PORT}`) // Log server start message
 })
-
